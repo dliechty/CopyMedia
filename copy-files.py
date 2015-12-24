f96be8e4cb01291ed345dc7aa1386ed0d0d99919
David Liechty <qwertyshoe@gmail.com>
2015-12-23 18:56:32 -0800

    Scan a file or a directory to match the file patterns
    If a file is specified, then compare that file against the patterns for
    each entry in the config file to determine a match. If no files are
    specified, then compare all files in the scan directory against the
    patterns and act on each identified file in sequence.

--- copy-files.py
+++ copy-files.py
@@ -4,6 +4,10 @@ import argparse
 import logging
 import subprocess
 import platform
+import json
+import re
+from os import listdir
+from os.path import isfile, join
 
 # Set up default file locations for configs and logs
 CONFIG_FILE = 'D:/Downloads/CopyAnime.json'
@@ -34,7 +38,49 @@ def main():
     logging.basicConfig(filename=getPath(args.log),
                         level=logLevel, format=FORMAT, filemode='a')
 
-    logging.debug('Using configuration file: %s', args.config)
+    logging.debug('Using configuration file: [%s]', args.config)
+
+    with open(args.config) as configFile:
+        config = json.load(configFile)
+
+        # if hasFile is true, then only check that one file
+        # against the configs. If file is not set, then scan
+        # the entire directory for files
+        hasFile = False
+        if args.file:
+            hasFile = True
+
+        if hasFile:
+            logging.debug('Scanning file [%s]', args.file)
+        else:
+            scanDir = args.scan
+            if config['scanDir']:
+                scanDir = config['scanDir']
+            logging.debug('Scanning Directory: [%s]', scanDir)
+
+        # Retrieve destination parent directory
+        moveDir = args.dest
+        if config['moveDir']:
+            moveDir = config['moveDir']
+        logging.debug('Destination Parent Directory: [%s]', moveDir)
+
+        files = []
+        if hasFile:
+            files.append(args.file)
+        else:
+            files = [f for f in listdir(scanDir) if isfile(join(scanDir, f))]
+
+        for f in files:
+            for show in config['series']:
+                logging.debug('Checking [%s] against [%s] using pattern [%s]',
+                              f, show['name'], show['regex'])
+                if show['regex']:
+                    if re.match(show['regex'], f):
+                        logging.info('++++++ File [%s] matches entry [%s]',
+                                     f, show['name'])
+                else:
+                    logging.error('[%s] has no regex pattern defined.',
+                                  show['name'])
 
 
 def getPath(path):
