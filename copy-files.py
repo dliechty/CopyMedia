c95f44dc9177b69318d76ce6cce1abff8b642e79
David Liechty <qwertyshoe@gmail.com>
2015-12-23 18:03:12 -0800

    Set default values for config file and log file in the appropriate way.

--- copy-files.py
+++ copy-files.py
@@ -3,6 +3,10 @@
 import argparse
 import logging
 
+# Set up default file locations for configs and logs
+CONFIG_FILE = 'D:\\Downloads\\CopyAnime.json'
+LOG_FILE = 'D:\\Downloads\\fileCopy.log'
+
 # Set up command line arguments
 argParser = argparse.ArgumentParser(description='Copy/transform large files,'
                                     ' then trigger plex media server to scan'
@@ -12,31 +16,23 @@ argParser.add_argument('-f', '--file', help='File to process')
 argParser.add_argument('-d', '--dest',
                        help='Destination parent directory')
 argParser.add_argument('-s', '--scan', help='Directory to scan')
-argParser.add_argument('-c', '--config', help='Configuration file')
-argParser.add_argument('-l', '--log', help='Log file')
-
-# Set up default file locations for configs and logs
-configFile = 'D:\\Downloads\\CopyAnime.json'
-logFile = 'D:\\Downloads\\fileCopy.log'
+argParser.add_argument('-c', '--config', help='Configuration file',
+                       default=CONFIG_FILE)
+argParser.add_argument('-l', '--log', help='Log file', default=LOG_FILE)
 
 logLevel = logging.DEBUG
-FORMAT = '%(asctime)-15s:%(levelname)s:%(message)s'
+FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 
 
 def main():
     global configFile
     global logFile
     args = argParser.parse_args()
-    if args.config:
-        configFile = args.config
 
-    if args.log:
-        logFile = args.log
-
-    logging.basicConfig(filename=logFile,
+    logging.basicConfig(filename=args.log,
                         level=logLevel, format=FORMAT, filemode='a')
 
-    logging.debug('Using configuration file: %s', str(configFile))
+    logging.debug('Using configuration file: %s', args.config)
 
 if __name__ == '__main__':
     main()
