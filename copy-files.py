82af261ed13460be842073eafd9f0431f9604474
David Liechty <qwertyshoe@gmail.com>
2016-07-16 11:46:16 -0700

    disable plex scan because plex is monitoring filesystem for changes now.

--- copy-files.py
+++ copy-files.py
@@ -101,7 +101,7 @@ def main():
 
         # Trigger plex scan in either the entire library or the specific
         # folder associated with the specified file
-        scanPlex(args.plexlibrary, destinations)
+        # scanPlex(args.plexlibrary, destinations)
 
 
 def moveFiles(matches, moveDir, scanDir):
