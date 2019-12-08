9f98471ebb787df2ffc789b41ca9a932e62f9dab
David Liechty <qwertyshoe@gmail.com>
2019-12-07 22:59:26 -0800

    Re-factor main executable into class with arguments to enable better testing.

--- copy-files.py
+++ copy-files.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 
 import argparse
-import logging
-import subprocess
-import platform
 import json
+import logging
+import platform
 import re
-import requests
+import shutil
+import subprocess
 from os import listdir, path, makedirs
 from os.path import isfile, join, split
-import shutil
+
+import requests
+
 import MyLogger
 
 logging.setLoggerClass(MyLogger)
 
 # Set up default file locations for configs and logs
-CONFIG_FILE = '/home/david/CopyMedia/CopyMedia.json'
-LOG_FILE = '/home/david/copy-files.log'
+CONFIG_FILE = './CopyMedia.json'
+LOG_FILE = './copy-files.log'
 
 IFTTT_URL_BASE = 'https://maker.ifttt.com/trigger'
 
@@ -42,177 +44,213 @@ logLevel = logging.DEBUG
 FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 
 
-def main():
-    args = argParser.parse_args()
+class CopyMedia:
 
-    logging.basicConfig(filename=get_path(args.log),
-                        level=logLevel, format=FORMAT, filemode='a')
+    file = None
+    logfile = None
+    configs = None
+    ifttt_url = None
+    scandir = None
+    destdir = None
 
-    logging.debug('Using configuration file: [%s]', args.config)
+    series = None
 
-    logging.debug('All command line arguments: [%s]', args)
+    def __init__(self, logfile, configs, ifttt_url, scandir, destdir, file):
+        self.file = file
+        self.logfile = logfile
+        self.configs = configs
+        self.ifttt_url = ifttt_url
+        self.scandir = scandir
+        self.destdir = destdir
 
-    with open(args.config) as configFile:
-        config = json.load(configFile)
+        if self.logfile is None:
+            self.logfile = LOG_FILE
 
-        # if an individual file is specified either by
-        # deluge or via the command line, then just use that.
-        # Otherwise, look for a directory to scan and scan the
-        # entire folder for matching files.
+        logging.basicConfig(filename=self.get_path(self.logfile),
+                            level=logLevel, format=FORMAT, filemode='a')
 
-        file = None
-        torrent_name = None
-        torrent_path = None
-        trigger_url = None
-        if args.delugeArgs and len(args.delugeArgs) == 4:
-            torrent_name = args.delugeArgs[1]
-            torrent_path = args.delugeArgs[2]
-            trigger_url = IFTTT_URL_BASE + '/' + args.delugeArgs[3]
+        self.process_configs()
 
-        if args.ifttt and trigger_url is None:
-            trigger_url = IFTTT_URL_BASE + '/' + args.ifttt
-
-        # set base file path based on deluge args if they exist
-        if torrent_name and torrent_path:
-            file = join(torrent_path, torrent_name)
-        # over-ride with explicit filepath from cmd if available
-        if args.file:
-            file = args.file
-
-        if file:
-            logging.debug('Scanning file [%s]', file)
-        else:
-            # Check config for scan_dir first
-            if 'scanDir' in config:
-                scan_dir = config['scanDir']
-            # Override if specified as command line arg
-            if args.scan:
-                scan_dir = args.scan
-            logging.debug('Scanning Directory: [%s]', scan_dir)
-
-        if not file and not scan_dir:
-            logging.exception('Must either specify a file or '
-                              'a directory to scan.')
-
-        # get destination directory from configs first
-        if 'moveDir' in config:
-            move_dir = config['moveDir']
-        # over-ride from cmd if available
-        if args.dest:
-            move_dir = args.dest
-
-        if move_dir:
-            logging.debug('Destination Parent Directory: [%s]', move_dir)
-        else:
-            logging.exception('Destination directory must be specified, '
-                              'either on the command line or in the '
-                              'configuration file.')
+    def execute(self):
 
         # Build list of files based on whether a single file has been
         # specified or whether we need to scan a directory
         files = []
-        if file:
-            scan_dir, file_name = split(file)
+        if self.file:
+            scan_dir, file_name = split(self.file)
             files.append(file_name)
         else:
-            files = [f for f in listdir(scan_dir) if isfile(join(scan_dir, f))]
+            files = [f for f in listdir(self.scandir) if isfile(join(self.scandir, f))]
 
         # Find matching files
-        matches = match_files(files, config['series'])
+        matches = self.match_files(files, self.series)
 
         if matches:
             # Move matching files to their respective destination directories
-            move_files(matches, move_dir, scan_dir)
+            self.move_files(matches, self.destdir, self.scandir)
 
             # Send notification to phone
-            send_notification(matches, trigger_url)
+            self.send_notification(matches, self.ifttt_url)
 
+    def process_configs(self):
 
-def send_notification(matches, trigger_url):
-    """Send IFTTT notification to phone whenever the script fires with the names
-        of the new episodes"""
+        if self.configs is None:
+            self.configs = CONFIG_FILE
 
-    # Only send notification if there is at least one matching file.
-    if matches:
-        # Get series name for each matching file and concatenate
-        # into a string separated by ' and '
-        names = [config['name'] for file, config in matches]
-        name_string = ' and '.join(names)
+        logging.debug('Using configuration file: [%s]', self.configs)
 
-        logging.debug('Sending notification with name string: [%s] to IFTTT',
-                      name_string)
+        with open(self.configs) as configfile:
+            config = json.load(configfile)
 
-        r = requests.post(trigger_url, data={'value1': name_string})
-        logging.debug('IFTTT POST status: [%s] with reason: [%s]',
-                      r.status_code, r.reason)
+            # if an individual file is specified either by
+            # deluge or via the command line, then just use that.
+            # Otherwise, look for a directory to scan and scan the
+            # entire folder for matching files.
 
-
-def move_files(matches, move_dir, scan_dir):
-    """Move matching files to their respective destination directory"""
-
-    destinations = set()
-
-    for file_name, config_entry in matches:
-
-        # Determine destination file_name if a replace attribute
-        # was specified
-        dest_file_name = file_name
-        if 'replace' in config_entry:
-            dest_file_name = re.sub(config_entry['regex'],
-                                    config_entry['replace'],
-                                    file_name)
-            logging.debug('New name for [%s] will be [%s]',
-                          file_name, dest_file_name)
-
-        # Build destination directory path
-        if 'destination' in config_entry:
-            dest = join(move_dir, config_entry['destination'])
-        else:
-            dest = join(move_dir, config_entry['name'])
-
-        # Create destination directory if it doesn't already exist
-        if not path.exists(dest):
-            logging.info('Destination does not exist; creating [%s]', dest)
-            makedirs(dest)
-
-        # Move file to destination folder, renaming on the way
-        logging.debug('Moving [%s] to [%s]...',
-                      join(scan_dir, file_name), join(dest, dest_file_name))
-        shutil.move(join(scan_dir, file_name), join(dest, dest_file_name))
-        logging.info('Successfully moved [%s] to [%s]',
-                     join(scan_dir, file_name), join(dest, dest_file_name))
-
-        destinations.add(dest)
-
-    return destinations
-
-
-def match_files(files, series):
-    """Find matching files given a list of files and a list of series."""
-
-    matches = []
-    for f in files:
-        for show in series:
-            logging.trace('Checking [%s] against [%s] using pattern [%s]',
-                          f, show['name'], show['regex'])
-            if show['regex']:
-                if re.match(show['regex'], f):
-                    matches.append((f, show))
-                    logging.info('File [%s] matches series [%s]',
-                                 f, show['name'])
-                    break
+            if self.file:
+                logging.debug('Found file to match [%s]', self.file)
             else:
-                logging.error('[%s] has no regex pattern defined.',
-                              show['name'])
-    return matches
+                # Check config for scan_dir first
+                if self.scandir is None and 'scanDir' in config:
+                    self.scandir = config['scanDir']
+                logging.debug('Found directory to scan: [%s]', self.scandir)
+
+            if not self.file and not self.scandir:
+                logging.exception('Must either specify a file or '
+                                  'a directory to scan.')
+
+            # get destination directory from configs first
+            if self.destdir is None and 'moveDir' in config:
+                self.destdir = config['moveDir']
+
+            if self.destdir:
+                logging.debug('Destination Parent Directory: [%s]', self.destdir)
+            else:
+                logging.exception('Destination directory must be specified, '
+                                  'either on the command line or in the '
+                                  'configuration file.')
+
+            logging.debug('Full IFTTT URL: [%s]', self.ifttt_url)
+
+            if 'series' in config:
+                self.series = config['series']
+            else:
+                logging.warning('No series configured.')
+
+    @staticmethod
+    def send_notification(self, matches, trigger_url):
+        """Send IFTTT notification to phone whenever the script fires with the names
+            of the new episodes"""
+
+        # Only send notification if there is at least one matching file.
+        if matches:
+            # Get series name for each matching file and concatenate
+            # into a string separated by ' and '
+            names = [config['name'] for file, config in matches]
+            name_string = ' and '.join(names)
+
+            logging.debug('Sending notification with name string: [%s] to IFTTT',
+                          name_string)
+
+            r = requests.post(trigger_url, data={'value1': name_string})
+            logging.debug('IFTTT POST status: [%s] with reason: [%s]',
+                          r.status_code, r.reason)
+
+    @staticmethod
+    def move_files(self, matches, move_dir, scan_dir):
+        """Move matching files to their respective destination directory"""
+
+        destinations = set()
+
+        for file_name, config_entry in matches:
+
+            # Determine destination file_name if a replace attribute
+            # was specified
+            dest_file_name = file_name
+            if 'replace' in config_entry:
+                dest_file_name = re.sub(config_entry['regex'],
+                                        config_entry['replace'],
+                                        file_name)
+                logging.debug('New name for [%s] will be [%s]',
+                              file_name, dest_file_name)
+
+            # Build destination directory path
+            if 'destination' in config_entry:
+                dest = join(move_dir, config_entry['destination'])
+            else:
+                dest = join(move_dir, config_entry['name'])
+
+            # Create destination directory if it doesn't already exist
+            if not path.exists(dest):
+                logging.info('Destination does not exist; creating [%s]', dest)
+                makedirs(dest)
+
+            # Move file to destination folder, renaming on the way
+            logging.debug('Moving [%s] to [%s]...',
+                          join(scan_dir, file_name), join(dest, dest_file_name))
+            shutil.move(join(scan_dir, file_name), join(dest, dest_file_name))
+            logging.info('Successfully moved [%s] to [%s]',
+                         join(scan_dir, file_name), join(dest, dest_file_name))
+
+            destinations.add(dest)
+
+        return destinations
+
+    @staticmethod
+    def match_files(self, files, series):
+        """Find matching files given a list of files and a list of series."""
+
+        matches = []
+        for f in files:
+            for show in series:
+                logging.trace('Checking [%s] against [%s] using pattern [%s]',
+                              f, show['name'], show['regex'])
+                if show['regex']:
+                    if re.match(show['regex'], f):
+                        matches.append((f, show))
+                        logging.info('File [%s] matches series [%s]',
+                                     f, show['name'])
+                        break
+                else:
+                    logging.error('[%s] has no regex pattern defined.',
+                                  show['name'])
+        return matches
+
+    @staticmethod
+    def get_path(self, argpath):
+        """Convert path to cygwin format if running on a cygwin platform"""
+
+        if 'CYGWIN' in platform.system():
+            argpath = subprocess.getoutput('cygpath ' + argpath)
+        return argpath
 
 
-def get_path(path):
-    """Convert path to cygwin format if running on a cygwin platform"""
+def main():
+    args = argParser.parse_args()
+    print('All command line arguments: ' + args)
 
-    if 'CYGWIN' in platform.system():
-        path = subprocess.getoutput('cygpath ' + path)
-    return path
+    file = None
+    torrent_name = None
+    torrent_path = None
+    trigger_url = None
+    if args.delugeArgs and len(args.delugeArgs) == 4:
+        torrent_name = args.delugeArgs[1]
+        torrent_path = args.delugeArgs[2]
+        trigger_url = IFTTT_URL_BASE + '/' + args.delugeArgs[3]
+
+    if args.ifttt and trigger_url is None:
+        trigger_url = IFTTT_URL_BASE + '/' + args.ifttt
+
+    # set base file path based on deluge args if they exist
+    if torrent_name and torrent_path:
+        file = join(torrent_path, torrent_name)
+    # over-ride with explicit filepath from cmd if available
+    if args.file:
+        file = args.file
+
+    # Now execute file transforms/copy
+    c = CopyMedia(args.log, args.config, trigger_url, args.scan, args.dest, file)
+    c.execute()
 
 
 if __name__ == '__main__':
