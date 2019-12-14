d2390dae5532ce8cd3a857cef82426e86f551d53
David Liechty <qwertyshoe@gmail.com>
2019-12-14 11:36:33 -0800

    Parse response from tmdb query and check the number of results. Improve logging.

--- tmdb.py
+++ tmdb.py
@@ -1,12 +1,26 @@
+import json
 import logging
 import urllib
 
 import requests
 
+import logger
+
 URL_CONTEXT = '/3/search/movie?api_key=API_KEY&include_adult=false&query=QUERY_STRING'
 DNS_NAME = 'api.themoviedb.org'
 PROTOCOL = 'https://'
 BASE_URL = PROTOCOL + DNS_NAME + URL_CONTEXT
+
+
+def clean_name(name):
+    """Used to clean up the name of the media so that it is suitable to send in an API query"""
+
+    logging.log(logger.TRACE, 'Raw name: [%s]', name)
+
+    # TODO: clean up name
+
+    logging.log(logger.TRACE, 'Cleaned name: [%s]', name)
+    return name
 
 
 def is_movie(name, api_key):
@@ -16,7 +30,12 @@ def is_movie(name, api_key):
     # Only send query if the media name and api key are provided
     if name and api_key:
 
-        url = BASE_URL.replace('QUERY_STRING', urllib.parse.quote(name))
+        logging.debug('Performing query to the movie DB with media name [%s]', name)
+
+        c_name = clean_name(name)
+        enc_name = urllib.parse.quote(clean_name(c_name))
+        logging.log(logger.TRACE, 'URL encoded name: [%s]', enc_name)
+        url = BASE_URL.replace('QUERY_STRING', enc_name)
 
         logging.debug('Sending query to [%s] TMDB with URL: [%s]', DNS_NAME, url)
 
@@ -26,3 +45,13 @@ def is_movie(name, api_key):
 
         logging.debug('TMDB GET status: [%s] with reason: [%s]',
                       r.status_code, r.reason)
+        logging.log(logger.TRACE, 'Results: [%s]', r.text)
+
+        if r.text:
+            result = json.loads(r.text)
+            num_results = result['total_results']
+            logging.debug('Number of results found: [%d]', num_results)
+            if result['total_results'] > 0:
+                return True
+
+        return False
